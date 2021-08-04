cd66490a20cddb5fe69cc1b7d88685dfa8f6c578
Rimma <tetradir@yandex.ru>
2021-08-04 14:01:19 +0300

    debug=false

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = 'h56$u)uq#=pf(hb&e@yxnou4k!#c-0hn%nu6*pd5yv=#5ond=6'
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     "*",
